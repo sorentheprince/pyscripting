d2d5d869863574c6d45a295d01f64e54b4d28b13
sorentheprince <xxvampxxify@gmail.com>
2023-06-19 00:12:57 +0800

    Made a script that would accept two integers and display a message based on the highest value.

---
+++ compare.py
@@ -0,0 +1,9 @@
+first = int(input("Enter a first number: "))
+second = int(input("Enter the second number: "))
+
+if first > second:
+    print("The first number is bigger.")
+elif second > first:
+    print("The second number is bigger.")
+else:
+    print("Both number are equal.")
